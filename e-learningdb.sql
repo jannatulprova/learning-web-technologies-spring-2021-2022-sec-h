3919cfaf4a6f02a3ba35117d2a4563887d196f6b
unknown <jannatulprova.99@gmail.com>
2022-03-29 00:10:19 +0600

    Project Database

---
+++ e-learningdb.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 28, 2022 at 08:05 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `e-learningdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course fee`
+--
+
+CREATE TABLE `course fee` (
+  `Teacher Name` varchar(50) NOT NULL,
+  `Course Name` varchar(50) NOT NULL,
+  `Course Fee` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `Student name` varchar(50) NOT NULL,
+  `Student ID` int(50) NOT NULL,
+  `Course Name` varchar(50) NOT NULL,
+  `Payment` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `Name` varchar(50) NOT NULL,
+  `E-mail` varchar(50) NOT NULL,
+  `Employee ID` int(50) NOT NULL,
+  `Username` varchar(50) NOT NULL,
+  `Password` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
